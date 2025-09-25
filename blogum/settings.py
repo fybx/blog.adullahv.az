b5db4195811fd878f4c383823c6d2cf0df1224aa
Yigid BALABAN <fyb@fybx.dev>
2025-09-25 13:39:26 +0300

    archive: dockerize

--- blogum/settings.py
+++ blogum/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$w^lvsj_=c3*mn-ka1=+!v-2t)9g81wdgc59)okak#7=ecve8@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 
@@ -137,6 +137,3 @@ CKEDITOR_CONFIGS = {
 MEDIA_URL = "/media/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-
-
